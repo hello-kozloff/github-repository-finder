b79080ca4b759997850bd01ff4373e1191ff37b2
hello_kozloff <hello.kozloff@gmail.com>
2020-06-10 17:49:48 +0300

    create search form component

---
+++ src/forms/SearchForm/index.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent, useState, ChangeEvent } from 'react';
+import { IProps } from './types';
+import { Input, PageHeader } from '../../components';
+
+/**
+ * Search Form
+ * @constructor
+ */
+const SearchForm: FunctionComponent<IProps> = ({
+  search
+}: IProps) => {
+  const [searchText, setSearchText] = useState(search || '');
+
+  /**
+   * The handle change input event.
+   * @param event
+   */
+  function handleChangeInput(event: ChangeEvent<HTMLInputElement>) {
+    const {
+      target: {
+        value
+      }
+    } = event;
+
+    setSearchText(value);
+  }
+
+  return (
+    <PageHeader
+      title="Поиск репозиториев GitHub"
+      subtitle="Находи, добавляй в закладки, изучай!"
+    >
+      <Input
+        variant="primary"
+        value={searchText}
+        placeholder="Введите название репозитория"
+        onChange={handleChangeInput}
+      />
+    </PageHeader>
+  );
+};
+
+export default SearchForm;
