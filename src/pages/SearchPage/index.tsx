819a595ce0a4b4f5cdb15fdad7d212df6f8e3767
hello_kozloff <hello.kozloff@gmail.com>
2020-06-10 16:15:23 +0300

    create search page component

---
+++ src/pages/SearchPage/index.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from 'react';
+
+/**
+ * Search Page Component
+ * @constructor
+ */
+const SearchPage: FunctionComponent = () => {
+    return (
+        <div>
+            <h1>SearchPage</h1>
+        </div>
+    );
+};
+
+export default SearchPage;
