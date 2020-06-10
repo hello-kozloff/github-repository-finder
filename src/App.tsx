8a4381078b73e88dad0c7080a5c80632462ed26f
hello_kozloff <hello.kozloff@gmail.com>
2020-06-10 16:18:10 +0300

    create browser router and connect search page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Pages
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>github-repository-finder</h1>
-    </div>
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={SearchPage} />
+        </Switch>
+    </BrowserRouter>
   );
 }
 
