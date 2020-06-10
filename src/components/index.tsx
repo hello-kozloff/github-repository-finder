df0e52d0cbdc963edc73a77f9c0a4860267027da
hello_kozloff <hello.kozloff@gmail.com>
2020-06-10 19:23:15 +0300

    export all components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,4 @@
 export { default as Input } from './Input';
 export { default as PageHeader } from './PageHeader';
+export { default as RepositoryCard } from './RepositoryCard';
+export { default as Navigation } from './Navigation';
