4f49f8f3657c097ac1ca4da2e4b3bbc98373b898
hello_kozloff <hello.kozloff@gmail.com>
2020-06-12 21:54:40 +0300

    add recent search section

--- src/pages/FavoritesPage/index.tsx
+++ src/pages/FavoritesPage/index.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { block } from 'bem-cn';
-import { PageHeader, Navigation, RepositoryCard, RepositoriesGrid } from '../../components';
+import {PageHeader, Navigation, RepositoryCard, RepositoriesGrid, RecentSearch} from '../../components';
 import { getFeaturesState } from '../../redux/reducers/features';
 import IStore from '../../redux/types/store';
 import { IRepository } from '../../redux/types/repository';
@@ -33,34 +33,40 @@ const FavoritesPage: FunctionComponent<IProps> = ({
 
       <div className={styleSheet('content')}>
 
-        <h2 className={styleSheet('title')}>
-          Избранные репозитории
-        </h2>
+        <div className={styleSheet('recent-search')}>
+          <RecentSearch />
+        </div>
 
-        {!features.length ? (
-          <p className={styleSheet('description')}>
-            В поиске вы можете добавить избранные репозитории!
-          </p>
-        ) : null}
+        <div className={styleSheet('favorites')}>
+          <h2 className={styleSheet('title')}>
+            Избранные репозитории
+          </h2>
 
-        <div className={styleSheet('grid')}>
-          <RepositoriesGrid>
-            {features.map((feature: IRepository) => (
-              <RepositoryCard
-                key={feature.id}
-                id={feature.id}
-                name={feature.name}
-                full_name={feature.full_name}
-                description={feature.description}
-                html_url={feature.html_url}
-                owner={{
-                  id: feature.owner.id,
-                  login: feature.owner.login,
-                  avatar_url: feature.owner.avatar_url
-                }}
-              />
-            ))}
-          </RepositoriesGrid>
+          {!features.length ? (
+            <p className={styleSheet('description')}>
+              В поиске вы можете добавить избранные репозитории!
+            </p>
+          ) : null}
+
+          <div className={styleSheet('grid')}>
+            <RepositoriesGrid>
+              {features.map((feature: IRepository) => (
+                <RepositoryCard
+                  key={feature.id}
+                  id={feature.id}
+                  name={feature.name}
+                  full_name={feature.full_name}
+                  description={feature.description}
+                  html_url={feature.html_url}
+                  owner={{
+                    id: feature.owner.id,
+                    login: feature.owner.login,
+                    avatar_url: feature.owner.avatar_url
+                  }}
+                />
+              ))}
+            </RepositoriesGrid>
+          </div>
         </div>
 
       </div>
